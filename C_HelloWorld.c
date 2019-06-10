115012720a4e4b08b52460de2b433aac629d9f0b
pzj <997464566@qq.com>
2019-06-10 19:38:40 +0800

    first commit

---
+++ C_HelloWorld.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+  printf("HelloWord");
+  return 0;
+}
---
+++ JAVA_HelloWorld.java
@@ -0,0 +1,10 @@
+public class JAVA_HelloWorld{
+
+    public static void main(String[] args) {
+
+	String a="HelloWorld";
+	System.out.println(a);
+
+    }
+
+}
